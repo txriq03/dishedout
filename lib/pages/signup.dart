3d9a639bb0b1efe65fda3b7b4f1d6cd2cae6ef0c
Tariq <118397532+txriq03@users.noreply.github.com>
2025-03-08 12:37:46 +0000

    feat: image and text added to signup page

--- lib/pages/signup.dart
+++ lib/pages/signup.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class Signup extends StatelessWidget {
   const Signup({ Key? key }) : super(key: key);
@@ -6,7 +7,42 @@ class Signup extends StatelessWidget {
   @override
   Widget build(BuildContext context){
     return MaterialApp(
-      
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: Padding(
+          padding: EdgeInsets.only(top: 50, left: 20, right: 20),
+          child: Column(
+            children: [
+             // This is the image
+            Container(
+              height: 400,
+              child: SvgPicture.asset(
+                'assets/signup.svg',
+                fit: BoxFit.fitWidth,
+              )
+            ),
+
+            Text(
+              "Sign up",
+              style: TextStyle(
+                fontSize: 32,
+                fontFamily: "Poppins",
+                fontWeight: FontWeight.bold
+              )
+            ),
+            Text(
+              "Let's first get your name!",
+              style: TextStyle(
+                fontFamily: "Poppins",
+                fontSize: 21,
+                color: Colors.grey[500]
+              )
+            )
+          ]
+          )
+        )
+      )
     );
   }
 }
