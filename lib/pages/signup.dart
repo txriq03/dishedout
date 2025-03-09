791b345bf6f37606e67041dd1229958faaefa34b
Tariq <118397532+txriq03@users.noreply.github.com>
2025-03-09 14:22:31 +0000

    feat: redeveloped sign in page

--- lib/pages/signup.dart
+++ lib/pages/signup.dart
@@ -21,13 +21,18 @@ class Signup extends StatelessWidget {
                       Positioned(
                         top: 10, 
                         left: 16,
-                        child: BackButton(color: Colors.white)
+                        child: BackButton(
+                          color: Colors.white,
+                          onPressed: () {
+                            Navigator.pop(context);
+                          }
+                        )
                       )
                     ],
                   ),
                 ),
                 Padding(
-                  padding: EdgeInsetsDirectional.symmetric(vertical: 50, horizontal: 15),
+                  padding: EdgeInsetsDirectional.symmetric(vertical: 50, horizontal: 25),
                   child: Column(
                     children: [
                       Text(
@@ -46,7 +51,11 @@ class Signup extends StatelessWidget {
                           fontSize: 24,
                           color: Colors.grey[300]
                         ),
-                      )
+                      ),
+                      SizedBox(height: 20),
+                      SignupForm()
+
+
                     ],
                   )
                 )
@@ -69,8 +78,9 @@ class SignupForm extends StatefulWidget {
 
 class _SignupFormState extends State<SignupForm> {
   final GlobalKey<FormState> _formGlobalKey = GlobalKey<FormState>();
-  final Color primaryColor = Colors.deepOrange;
-  final Color accentColor = Color.fromARGB(106, 255, 204, 188);
+  final Color accentColor = Colors.deepOrange[400] as Color;
+  final Color primaryColor = Color.fromARGB(106, 255, 204, 188);
+  final Color backgroundFieldColor = Color.fromARGB(255, 49, 49, 49);
 
   String? isFieldValid(String? value, String type) {
     if (value == null || value.isEmpty) {
@@ -95,120 +105,201 @@ class _SignupFormState extends State<SignupForm> {
 
           // Input fields
           TextFormField(
-            cursorColor: primaryColor,
+            cursorColor: Colors.deepOrange,
+            style: TextStyle(color: Colors.white),
             decoration: InputDecoration(
+              prefixIcon: Icon(
+                Icons.account_circle,
+                color: Colors.grey[700],
+                semanticLabel: 'Input display name here',
+              ),
               enabledBorder: UnderlineInputBorder(
-                borderSide: BorderSide(color: primaryColor)
+                borderSide: BorderSide(color: Colors.transparent),
+                borderRadius: BorderRadius.circular(12)
               ),
               focusedBorder: UnderlineInputBorder(
-                borderSide: BorderSide(color: primaryColor)
+                borderSide: BorderSide(color: Colors.transparent),
+                borderRadius: BorderRadius.circular(12)
               ),
               filled: true,
-              fillColor: accentColor,
-              labelText: 'Username',
+              fillColor: backgroundFieldColor,
+              labelText: 'Display name',
               labelStyle: TextStyle(
-                fontSize: 18,
-                color: Colors.grey[700],
+                color: Colors.grey[600],
                 fontFamily: 'Poppins'
               ),
-              floatingLabelStyle: TextStyle(
-                color: primaryColor,
-              )
-            ),
-            validator: (value) => isFieldValid(value, "username")
-          ),
-          SizedBox(height: 15),
-          TextFormField(
-            cursorColor: primaryColor,
-            decoration: InputDecoration(
-              enabledBorder: UnderlineInputBorder(
-                borderSide: BorderSide(color: primaryColor)
-              ),
-              focusedBorder: UnderlineInputBorder(
-                borderSide: BorderSide(color: primaryColor)
-              ),
-              filled: true,
-              fillColor: accentColor,
-              labelText: 'Email',
-              labelStyle: TextStyle(
-                fontSize: 18,
-                color: Colors.grey[700],
-                fontFamily: 'Poppins'
-              ),
-              floatingLabelStyle: TextStyle(
-                color: primaryColor,
-              )
             ),
             validator: (value) => isFieldValid(value, "email")
           ),
           SizedBox(height: 15),
           TextFormField(
-            cursorColor: primaryColor,
+            style: TextStyle(color: Colors.white),
+            cursorColor: Colors.deepOrange,
             decoration: InputDecoration(
+              prefixIcon: Icon(
+                Icons.alternate_email_rounded,
+                color: Colors.grey[700],
+                semanticLabel: 'Input email here',
+              ),
               enabledBorder: UnderlineInputBorder(
-                borderSide: BorderSide(color: primaryColor)
+                borderSide: BorderSide(color: Colors.transparent),
+                borderRadius: BorderRadius.circular(12)
               ),
               focusedBorder: UnderlineInputBorder(
-                borderSide: BorderSide(color: primaryColor)
+                borderSide: BorderSide(color: Colors.transparent),
+                borderRadius: BorderRadius.circular(12)
               ),
               filled: true,
-              fillColor: accentColor,
-              labelText: 'Password',
+              fillColor: backgroundFieldColor,
+              labelText: 'Email',
               labelStyle: TextStyle(
-                fontSize: 18,
-                color: Colors.grey[700],
+                color: Colors.grey[600],
                 fontFamily: 'Poppins'
               ),
-              floatingLabelStyle: TextStyle(
-                color: primaryColor,
+            )
+          ),
+          SizedBox(height: 15),
+          TextFormField(
+            style: TextStyle(color: Colors.white),
+            cursorColor: Colors.deepOrange,
+            decoration: InputDecoration(
+              prefixIcon: Icon(
+                Icons.lock_rounded,
+                color: Colors.grey[700],
+                semanticLabel: 'Input display name here',
+              ),
+              suffixIcon: Icon(
+                Icons.visibility_off_rounded,
+                color: Colors.grey[500],
+                semanticLabel: 'Input display name here',
+              ),
+              enabledBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.transparent),
+                borderRadius: BorderRadius.circular(12)
+              ),
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.transparent),
+                borderRadius: BorderRadius.circular(12)
+              ),
+              filled: true,
+              fillColor: backgroundFieldColor,
+              labelText: 'Password',
+              labelStyle: TextStyle(
+                color: Colors.grey[600],
+                fontFamily: 'Poppins'
               )
             ),
             validator: (value) => isFieldValid(value, "password")
           ),
           SizedBox(height: 15),
           TextFormField(
-            cursorColor: primaryColor,
+            style: TextStyle(color: Colors.white),
+            cursorColor: Colors.deepOrange,
             decoration: InputDecoration(
+              prefixIcon: Icon(
+                Icons.lock_rounded,
+                color: Colors.grey[700],
+                semanticLabel: 'Input display name here',
+              ),
+              suffixIcon: Icon(
+                Icons.visibility_off_rounded,
+                color: Colors.grey[500],
+                semanticLabel: 'Input display name here',
+              ),
               enabledBorder: UnderlineInputBorder(
-                borderSide: BorderSide(color: primaryColor)
+                borderSide: BorderSide(color: Colors.transparent),
+                borderRadius: BorderRadius.circular(12)
               ),
               focusedBorder: UnderlineInputBorder(
-                borderSide: BorderSide(color: primaryColor)
+                borderSide: BorderSide(color: Colors.transparent),
+                borderRadius: BorderRadius.circular(12)
               ),
               filled: true,
-              fillColor: accentColor,
+              fillColor: backgroundFieldColor,
               labelText: 'Confirm Password',
               labelStyle: TextStyle(
-                fontSize: 18,
-                color: Colors.grey[700],
+                color: Colors.grey[600],
                 fontFamily: 'Poppins'
-              ),
-              floatingLabelStyle: TextStyle(
-                color: primaryColor,
               )
             ),
             validator: (value) => isFieldValid(value, "password")
           ),
-          SizedBox(height: 10),
+          
+          SizedBox(height: 35),
+          
+          
+          // Sign up button
+          Container(
+            decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.deepOrange.withValues(alpha: 0.5),
+                  blurRadius: 15,
+                  spreadRadius: 0.1,
+                )
+              ],
+              borderRadius: BorderRadius.circular(25)
+            ),
+            child: FilledButton(
+              onPressed: () {
+                _formGlobalKey.currentState!.validate();
+              },
+              style: FilledButton.styleFrom(
+                minimumSize: Size(double.infinity, 55),
+                backgroundColor: Colors.deepOrange[400],
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(25)
+                )
+              ),
+              child: Text("Sign up", style: TextStyle(fontSize: 18))
+            ),
+          ),
+          
+          // Divider
+          Row(
+            children: [
+              Expanded(child: Divider(
+                color: Colors.grey[700],
+                thickness: 2,
+                
+              )),
+              Padding(
+                padding: EdgeInsetsDirectional.symmetric(horizontal: 10, vertical: 35),
+                child: Text(
+                  "Or continue with",
+                  style: TextStyle(
+                    fontFamily: 'Poppins',
+                    fontSize: 16,
+                    color: Colors.white
+                  ),
+                ),
+              ),
+              Expanded(child: Divider(
+                color: Colors.grey[700],
+                thickness: 2,
+              ))
+            ]
+          ),
 
-          // Button
-          FilledButton(
+          ElevatedButton(
             onPressed: () {
-              _formGlobalKey.currentState!.validate();
+
             },
-            style: FilledButton.styleFrom(
-              backgroundColor: Colors.deepOrange[400],
+            style: ElevatedButton.styleFrom(
+              backgroundColor: backgroundFieldColor,
+              minimumSize: Size(double.infinity, 55),
               shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(8)
+                borderRadius: BorderRadius.circular(15)
               )
             ),
             child: Text(
-              'Next',
+              'Continue with Google',
               style: TextStyle(
                 fontFamily: 'Poppins',
+                fontSize: 16,
                 color: Colors.white
-              )
-            ),
+              ),
+            )
           )
         ]
       )
