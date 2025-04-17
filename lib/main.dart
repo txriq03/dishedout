ac8beb90128b9fae4d83da6bff0b2acbf979d583
Tariq <118397532+txriq03@users.noreply.github.com>
2025-04-17 21:16:23 +0100

    fix: removed debug ribbon

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: StreamBuilder<User?>(
         stream: _auth.authStateChanges,
         builder: (context, snapshot) {
