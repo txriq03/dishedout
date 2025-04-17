285ea087713f5fe50a2a1e5434e52daf68de6371
Tariq <118397532+txriq03@users.noreply.github.com>
2025-04-17 22:00:12 +0100

    feat: created appbar in homepage

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -2,26 +2,51 @@ import 'package:flutter/material.dart';
 import 'package:dishedout/auth.dart';
 
 class HomePage extends StatelessWidget {
-  final Auth _auth = Auth(); // Reference to your custom Auth class
+  final Auth _auth = Auth();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        title: const Text("Welcome Home"),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.logout),
-            tooltip: 'Logout',
-            onPressed: () async {
-              await _auth.signOut();
-            },
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(80),
+        child: AppBar(
+          automaticallyImplyLeading: false, // Removes back button
+          title: Padding(
+            padding: EdgeInsets.only(top: 12.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Welcome back",
+                  style: TextStyle(
+                    color: Colors.grey[700],
+                    fontSize: 18,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+                Text(
+                  "Username",
+                  style: TextStyle(
+                    color: Colors.grey[200],
+                    fontSize: 21,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+              ],
+            ),
           ),
-        ],
-      ),
-      body: const Center(
-        child: Text('You are now logged in!', style: TextStyle(fontSize: 18)),
+
+          backgroundColor: Colors.grey[900],
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.logout),
+              tooltip: 'Logout',
+              onPressed: () async {
+                await _auth.signOut();
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
