cd36c00285721016ef86152be8050709ea8f5151
Tariq <118397532+txriq03@users.noreply.github.com>
2025-04-19 00:43:21 +0100

    style: added dotted line styling to upload image placeholder

--- lib/pages/image_upload.dart
+++ lib/pages/image_upload.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:dotted_border/dotted_border.dart';
 import 'dart:io';
 
 class ImageUpload extends StatefulWidget {
@@ -41,12 +42,19 @@ class _ImageUploadState extends State<ImageUpload> {
           const SizedBox(height: 20),
           _image != null
               ? Image.file(_image!, height: 200, width: 200, fit: BoxFit.cover)
-              : Container(
-                width: size,
-                height: size - 20,
-                decoration: BoxDecoration(
-                  color: Colors.grey[900],
-                  borderRadius: BorderRadius.circular(12),
+              : DottedBorder(
+                borderType: BorderType.RRect,
+                color: Colors.grey[800]!,
+                dashPattern: [6, 3],
+                radius: const Radius.circular(12),
+                strokeWidth: 2,
+                child: Container(
+                  width: size,
+                  height: size - 20,
+                  decoration: BoxDecoration(
+                    color: Colors.grey[900],
+                    borderRadius: BorderRadius.circular(12),
+                  ),
                 ),
               ),
           const SizedBox(height: 20),
