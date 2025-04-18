d7c22af8b1cd4c97a8d41634ef8901a14b2d56e4
Tariq <118397532+txriq03@users.noreply.github.com>
2025-04-18 02:42:12 +0100

    feat: added image and text to user upload placeholder in homepage

--- lib/pages/user_uploads.dart
+++ lib/pages/user_uploads.dart
@@ -15,17 +15,65 @@ class _UserUploadsState extends State<UserUploads> {
       child: Row(
         children: [
           Container(
-            width: 200,
-            height: 200,
+            width: 250,
+            height: 300,
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(12),
               color: Colors.grey[900],
+              image: DecorationImage(
+                image: AssetImage(
+                  'assets/pancake.jpg',
+                ), // Replace with your image path
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: Stack(
+              children: [
+                // Add a gradient overlay at the bottom
+                Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Container(
+                    height: 60, // Adjust the height of the dark overlay
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.vertical(
+                        bottom: Radius.circular(
+                          12,
+                        ), // Match the container's border radius
+                      ),
+                      gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [
+                          Colors.transparent,
+                          Colors.black.withValues(alpha: 1),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                // Add text on top of the gradient
+                Align(
+                  alignment: Alignment.bottomLeft,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      'Product name',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w200,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
+
           SizedBox(width: 10),
           Container(
-            width: 200,
-            height: 200,
+            width: 250,
+            height: 300,
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(12),
               color: Colors.grey[900],
