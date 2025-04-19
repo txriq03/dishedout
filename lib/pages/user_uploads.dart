18f3b23b445a5e57343c4e262b62f19ec8d71a53
Tariq <118397532+txriq03@users.noreply.github.com>
2025-04-19 01:23:42 +0100

    feat: replaced user uploads with carousel component

--- lib/pages/user_uploads.dart
+++ lib/pages/user_uploads.dart
@@ -8,77 +8,26 @@ class UserUploads extends StatefulWidget {
 }
 
 class _UserUploadsState extends State<UserUploads> {
+  final CarouselController controller = CarouselController(initialItem: 1);
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      scrollDirection: Axis.horizontal,
-      child: Row(
+    return ConstrainedBox(
+      constraints: const BoxConstraints(maxHeight: 400, minHeight: 200),
+      child: CarouselView.weighted(
+        controller: controller,
+        itemSnapping: true,
+        flexWeights: [8, 2],
         children: [
-          Container(
-            width: 250,
-            height: 300,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(12),
-              color: Colors.grey[900],
-              image: DecorationImage(
-                image: AssetImage(
-                  'assets/pancake.jpg',
-                ), // Replace with your image path
-                fit: BoxFit.cover,
-              ),
-            ),
-            child: Stack(
-              children: [
-                // Add a gradient overlay at the bottom
-                Align(
-                  alignment: Alignment.bottomCenter,
-                  child: Container(
-                    height: 60, // Adjust the height of the dark overlay
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.vertical(
-                        bottom: Radius.circular(
-                          12,
-                        ), // Match the container's border radius
-                      ),
-                      gradient: LinearGradient(
-                        begin: Alignment.topCenter,
-                        end: Alignment.bottomCenter,
-                        colors: [
-                          Colors.transparent,
-                          Colors.black.withValues(alpha: 1),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-                // Add text on top of the gradient
-                Align(
-                  alignment: Alignment.bottomLeft,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      'Product name',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 21,
-                        fontWeight: FontWeight.w200,
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-
-          SizedBox(width: 10),
-          Container(
-            width: 250,
-            height: 300,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(12),
-              color: Colors.grey[900],
-            ),
-          ),
+          Image.asset('assets/pancake.jpg', fit: BoxFit.cover),
+          Image.asset('assets/meat.jpg', fit: BoxFit.cover),
+          Image.asset('assets/spices.jpg', fit: BoxFit.cover),
         ],
       ),
     );
