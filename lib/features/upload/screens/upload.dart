0e7065b75f526cd903c47c48eb69db8036782d2c
Tariq <118397532+txriq03@users.noreply.github.com>
2025-04-19 18:34:12 +0100

    feat: added step enum with associated functions

--- lib/features/upload/screens/upload.dart
+++ lib/features/upload/screens/upload.dart
@@ -1,8 +1,47 @@
 import 'package:dishedout/features/upload/widgets/image_upload.dart';
 import 'package:flutter/material.dart';
 
-class UploadPage extends StatelessWidget {
+// Enum for handling what step of upload we use
+enum Step { takePhoto, fillForm, success }
+
+class UploadPage extends StatefulWidget {
   const UploadPage({super.key});
+
+  @override
+  State<UploadPage> createState() => _UploadPageState();
+}
+
+class _UploadPageState extends State<UploadPage> {
+  Step _currentStep = Step.takePhoto;
+
+  // Handles next step after button pressed
+  void _nextStep() {
+    setState(() {
+      switch (_currentStep) {
+        case Step.takePhoto:
+          _currentStep = Step.fillForm;
+          break;
+        case Step.fillForm:
+          _currentStep = Step.success;
+          break;
+        case Step.success:
+          // Handle success step if needed
+          break;
+      }
+    });
+  }
+
+  // What widget is shown based on the current step
+  Widget _getStepWidget() {
+    switch (_currentStep) {
+      case Step.takePhoto:
+        return const ImageUpload();
+      case Step.fillForm:
+        return const Text('Fill Form');
+      case Step.success:
+        return const Text('Success!');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,7 +63,7 @@ class UploadPage extends StatelessWidget {
                 ),
               ),
               const SizedBox(height: 20),
-              Expanded(child: ImageUpload()),
+              Expanded(child: _getStepWidget()),
             ],
           ),
         ),
