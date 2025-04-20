2ebff47a06a4ec5041b95a23f9e05af762ca9086
Tariq <118397532+txriq03@users.noreply.github.com>
2025-04-20 21:05:54 +0100

    feat: added app_colors.dart file

---
+++ lib/config/themes/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Color.fromARGB(255, 27, 27, 27);
+const Color secondaryColor = Colors.deepOrange;
+const Color backgroundColor = Color.fromARGB(255, 19, 19, 19);
+const Color textColor = Colors.white;
+const Color hintTextColor = Colors.grey;
+const Color inputFillColor = Colors.grey;
