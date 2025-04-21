b065ab889e50b2b2756481a623df3c739a0e879c
Tariq <118397532+txriq03@users.noreply.github.com>
2025-04-21 19:44:16 +0100

    fix: mismatch color with navbar and gap underneath it

--- lib/shared/widgets/navbar.dart
+++ lib/shared/widgets/navbar.dart
@@ -2,6 +2,7 @@ import 'package:dishedout/features/upload/screens/upload.dart';
 import 'package:dishedout/features/home/screens/homepage.dart';
 import 'package:dishedout/features/profile/screens/profile.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class Navbar extends StatefulWidget {
   const Navbar({super.key});
@@ -17,6 +18,18 @@ class _NavbarState extends State<Navbar> {
 
   @override
   Widget build(BuildContext context) {
+    // Necessary to get rid of the mismatch color below navbar
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(
+        systemNavigationBarColor:
+            Theme.of(
+              context,
+            ).colorScheme.surfaceContainer, // Change this to your desired color
+        systemNavigationBarIconBrightness:
+            Brightness.dark, // Adjust icon brightness
+      ),
+    );
+
     return Scaffold(
       body: _pages[_selectedIndex],
       bottomNavigationBar: NavigationBar(
