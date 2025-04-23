8a8ac39c4e71a3f11331604305c98dda73829f63
Tariq <118397532+txriq03@users.noreply.github.com>
2025-04-24 00:16:55 +0100

    feat: update post model to include address

--- lib/models/post_model.dart
+++ lib/models/post_model.dart
@@ -7,6 +7,9 @@ class Post {
   final String description;
   final String imageUrl;
   final DateTime createdAt;
+  final String addressDescription;
+  final double latitude;
+  final double longitude;
 
   Post({
     required this.id,
@@ -14,16 +17,23 @@ class Post {
     required this.name,
     required this.description,
     required this.imageUrl,
+    required this.addressDescription,
+    required this.latitude,
+    required this.longitude,
     required this.createdAt,
   });
 
   factory Post.fromMap(String id, Map<String, dynamic> data) {
+    final address = data['address'] ?? {};
     return Post(
       id: id,
       uid: data['uid'] ?? '',
       name: data['name'] ?? '',
       description: data['description'] ?? '',
       imageUrl: data['imageUrl'] ?? '',
+      addressDescription: address['addressDescription'] ?? '',
+      latitude: (address['latitude'] as num?)?.toDouble() ?? 0.0,
+      longitude: (address['longitude'] as num?)?.toDouble() ?? 0.0,
       createdAt: (data['createdAt'] as Timestamp?)?.toDate() ?? DateTime.now(),
     );
   }
@@ -35,6 +45,11 @@ class Post {
       'description': description,
       'imageUrl': imageUrl,
       'createdAt': Timestamp.fromDate(createdAt),
+      'address': {
+        'description': addressDescription,
+        'latitude': latitude,
+        'longitude': longitude,
+      },
     };
   }
 }
