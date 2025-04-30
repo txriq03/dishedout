0a38bcece9e84a28cbbc673b4cd9cc54495ed7dd
Tariq <118397532+txriq03@users.noreply.github.com>
2025-04-30 22:39:33 +0100

    feat: add subscribe banner to homepage

--- lib/features/home/widgets/subscribe_banner.dart
+++ lib/features/home/widgets/subscribe_banner.dart
@@ -5,15 +5,50 @@ class SubscribeBanner extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 75,
-      width: double.infinity,
-      padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(15),
-        color: Theme.of(context).colorScheme.surfaceContainer,
+    return Card(
+      elevation: 0,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+      child: InkWell(
+        borderRadius: BorderRadius.circular(20),
+        onTap: () {},
+        child: Container(
+          height: 90,
+          width: double.infinity,
+          padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Icon(Icons.workspace_premium, size: 35),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Want more meals?',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.w300),
+                  ),
+                  Text(
+                    'Become a premium member',
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Colors.white.withValues(alpha: 0.5),
+                    ),
+                  ),
+                ],
+              ),
+              Container(
+                padding: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.red.shade300,
+                ),
+                child: Icon(Icons.arrow_outward_rounded),
+              ),
+            ],
+          ),
+        ),
       ),
-      child: Text('Become a premium member', style: TextStyle(fontSize: 18)),
     );
   }
 }
