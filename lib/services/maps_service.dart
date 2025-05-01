69e07fe1ba8ec6a22bf53adbd0a8b58942e0d680
Tariq <118397532+txriq03@users.noreply.github.com>
2025-05-01 21:23:25 +0100

    feat: add locationSetings to enable background location tracking

--- lib/services/maps_service.dart
+++ lib/services/maps_service.dart
@@ -2,17 +2,28 @@ import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapService {
+  final LocationSettings locationSettings = AndroidSettings(
+    accuracy: LocationAccuracy.best,
+    distanceFilter: 10,
+    foregroundNotificationConfig: ForegroundNotificationConfig(
+      notificationTitle: 'Location Tracking Active',
+      notificationText: 'Tracking your location for lender...',
+      enableWakeLock: true,
+    ),
+  );
+  MapService();
+
   Stream<double> getDistanceStream(LatLng lenderLocation) {
-    return Geolocator.getPositionStream(
-      locationSettings: LocationSettings(accuracy: LocationAccuracy.best),
-    ).map((position) {
-      // Calculate distance
-      return Geolocator.distanceBetween(
-        position.latitude,
-        position.longitude,
-        lenderLocation.latitude,
-        lenderLocation.longitude,
-      );
-    });
+    return Geolocator.getPositionStream(locationSettings: locationSettings).map(
+      (position) {
+        // Calculate distance
+        return Geolocator.distanceBetween(
+          position.latitude,
+          position.longitude,
+          lenderLocation.latitude,
+          lenderLocation.longitude,
+        );
+      },
+    );
   }
 }
