9e050ebd9a5cb3209125761f1f042673ef177330
Tariq <118397532+txriq03@users.noreply.github.com>
2025-05-03 21:13:30 +0100

    Revert "feat: replace user_provider with StateNotifier"
    This reverts commit e768f764fc14ea8531b9a5321aa84ef3a97fb6b8.

--- lib/features/authentication/providers/user_provider.dart
+++ lib/features/authentication/providers/user_provider.dart
@@ -1,39 +1,17 @@
-import 'package:dishedout/services/user_service.dart';
+import 'package:dishedout/models/user_model.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:dishedout/models/user_model.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class UserController extends StateNotifier<UserModel?> {
-  UserController()
-    : super(null); // This is necessary when extending the class StateNotifier
+final currentUserProvider = FutureProvider<UserModel?>((ref) async {
+  final user = FirebaseAuth.instance.currentUser;
 
-  // Load user from firestore
-  Future<void> loadUser() async {
-    final UserService userService = UserService();
-    final User? currentUser = FirebaseAuth.instance.currentUser;
+  if (user == null) return null;
 
-    if (currentUser == null) {
-      state = null;
-      return;
-    }
+  final doc =
+      await FirebaseFirestore.instance.collection('users').doc(user.uid).get();
 
-    final UserModel? user = await userService.getUser(currentUser.uid);
+  if (!doc.exists) return null;
 
-    if (user == null) {
-      state = null;
-      return;
-    }
-
-    state = user;
-  }
-
-  // Clear user
-  void clearUser() {
-    state = null;
-  }
-
-  // Update user manually
-  void updateuser(UserModel user) {
-    state = user;
-  }
-}
+  return UserModel.fromDocument(doc);
+});
--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:dishedout/services/notification_service.dart';
 import 'package:dishedout/shared/widgets/navbar.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
-import 'package:dishedout/features/authentication/presentation/screens/auth_page.dart';
+import 'package:dishedout/features/authentication/screens/auth_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'firebase_options.dart';
