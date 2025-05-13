298c962d01f39dbd3fb26d1478227844866e1fb8
Tariq <118397532+txriq03@users.noreply.github.com>
2025-05-13 02:24:10 +0100

    :sparkles: create edit profile page

---
+++ lib/features/product/screens/edit_profile_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class EditProfilePage extends ConsumerStatefulWidget {
+  const EditProfilePage({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() =>
+      _EditProfilePageState();
+}
+
+class _EditProfilePageState extends ConsumerState<EditProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
