783297bf265d3fcaa84cfcbdf5cf58794eeafa52
Tariq <118397532+txriq03@users.noreply.github.com>
2025-05-15 21:58:28 +0100

    :sparkle: create ChatService

---
+++ lib/services/chat_service.dart
@@ -0,0 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class ChatService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+}
