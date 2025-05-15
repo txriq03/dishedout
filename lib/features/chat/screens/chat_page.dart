4549a7f24ae668191f5ff0747fa0cd08532f70eb
Tariq <118397532+txriq03@users.noreply.github.com>
2025-05-15 23:56:05 +0100

    :sparkles: add message field and button

--- lib/features/chat/screens/chat_page.dart
+++ lib/features/chat/screens/chat_page.dart
@@ -22,6 +22,45 @@ class _ChatPageState extends ConsumerState<ChatPage> {
         title: Text(widget.otherDisplayName, style: TextStyle(fontSize: 28)),
         centerTitle: true,
       ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Center(
+              child: Text(
+                "No messages yet :(",
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.white.withValues(alpha: 0.25),
+                ),
+              ),
+            ),
+          ),
+          SafeArea(
+            child: Container(
+              padding: EdgeInsets.symmetric(vertical: 10),
+              margin: EdgeInsets.symmetric(horizontal: 10),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      decoration: const InputDecoration(
+                        hintText: "Type a message...",
+                        border: OutlineInputBorder(),
+                        contentPadding: EdgeInsets.symmetric(
+                          horizontal: 20,
+                          vertical: 15,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 8),
+                  IconButton(icon: const Icon(Icons.send), onPressed: () {}),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
