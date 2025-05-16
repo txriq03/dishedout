76b3ffd240bcd978325a4f7274e272a2c31e8d8d
Tariq <118397532+txriq03@users.noreply.github.com>
2025-05-16 19:52:16 +0100

    :lipstick: changed UI for auth page

--- lib/features/authentication/screens/auth_page.dart
+++ lib/features/authentication/screens/auth_page.dart
@@ -9,6 +9,74 @@ class AuthPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     print('Authpage is being displayed');
-    return Scaffold(body: Center(child: Column(children: [])));
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            Lottie.asset('assets/animations/abstract.json'),
+            SizedBox(height: 5),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Welcome to ', style: TextStyle(fontSize: 21)),
+                Text(
+                  'DishedOut',
+                  style: TextStyle(
+                    fontSize: 21,
+                    color: Theme.of(context).colorScheme.primary,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                SizedBox(height: 5),
+              ],
+            ),
+            SizedBox(height: 5),
+            Text(
+              "Contribute to your community and reduce household waste.",
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.white.withValues(alpha: 0.6)),
+            ),
+            SizedBox(height: 5),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 30),
+              child: Column(
+                children: [
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: const Size.fromHeight(55),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                    onPressed: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(builder: (context) => const Login()),
+                      );
+                    },
+                    child: Text('Login', style: TextStyle(fontSize: 18)),
+                  ),
+                  SizedBox(height: 10),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: const Size.fromHeight(55),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                    onPressed: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(builder: (context) => const Signup()),
+                      );
+                    },
+                    child: Text('Sign up', style: TextStyle(fontSize: 18)),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
